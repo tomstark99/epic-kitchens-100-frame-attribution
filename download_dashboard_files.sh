b2971c4bbad171b70a572a6faa7b5281665af717
tomstark99 <jb18789@bristol.ac.uk>
2021-06-18 15:26:37 +0100

    updated download script

--- download_dashboard_files.sh
+++ download_dashboard_files.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
-set -eux
 
-download() {
-  wget "$1" -O "$2"
-}
-
-download "https://drive.google.com/file/d/1f6DO6UxH1OqW8s6Efdgne81yqRhkULNR/view?usp=sharing" dashboard_files.tar
+wget --load-cookies cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1f6DO6UxH1OqW8s6Efdgne81yqRhkULNR' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1f6DO6UxH1OqW8s6Efdgne81yqRhkULNR" -O dashboard_files.tar && rm -rf cookies.txt
 tar -xvf dashboard_files.tar
 rm dashboard_files.tar

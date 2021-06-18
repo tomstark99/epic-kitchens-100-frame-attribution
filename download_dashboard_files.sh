f7d7c6c36975f8a54643fa9d71ad0bba75c1a14d
tomstark99 <jb18789@bristol.ac.uk>
2021-06-18 14:14:59 +0100

    activated script

--- download_dashboard_files.sh
+++ download_dashboard_files.sh
@@ -5,8 +5,6 @@ download() {
   wget "$1" -O "$2"
 }
 
-# download "https://drive.google.com/file/d/1f6DO6UxH1OqW8s6Efdgne81yqRhkULNR/view?usp=sharing" dashboard_files.tar
+download "https://drive.google.com/file/d/1f6DO6UxH1OqW8s6Efdgne81yqRhkULNR/view?usp=sharing" dashboard_files.tar
 tar -xvf dashboard_files.tar
 rm dashboard_files.tar
-tree -h ./mtrn-*
-tree -h datasets/epic-100/labels

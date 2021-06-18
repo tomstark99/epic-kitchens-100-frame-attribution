27cf8c790e7a842dcf6c598d674e1011feeaecf1
tomstark99 <jb18789@bristol.ac.uk>
2021-06-18 13:27:05 +0100

    dashboard files

---
+++ download_dashboard_files.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -eux
+
+download() {
+  wget "$1" -O "$2"
+}
+
+# download "https://drive.google.com/file/d/1f6DO6UxH1OqW8s6Efdgne81yqRhkULNR/view?usp=sharing" dashboard_files.tar
+tar -xvf dashboard_files.tar
+rm dashboard_files.tar
+tree -h ./mtrn-*
+tree -h datasets/epic-100/labels
